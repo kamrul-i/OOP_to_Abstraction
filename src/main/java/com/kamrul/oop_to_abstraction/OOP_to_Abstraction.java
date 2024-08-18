e3cdf279ff0a23112177eb0b7e02f4bcc1f2d007
offic <offic@DESKTOP-GLF4UNU>
2024-08-18 22:49:41 +0600

    Welcome! Introduction to "OOP in Abstraction".

---
+++ src/main/java/com/kamrul/oop_to_abstraction/OOP_to_Abstraction.java
@@ -0,0 +1,8 @@
+package com.kamrul.oop_to_abstraction;
+public class OOP_to_Abstraction {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"object oriented programming_OOP\".");
+        
+    }
+}
